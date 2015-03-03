80bef40f38cf92f2ba2afeeb1bce663a475fd719
Marcel Rodrigues <marcelgmr@gmail.com>
2015-03-03 16:44:32 -0300

    Faster progress bar.

--- bar.lua
+++ bar.lua
@@ -3,11 +3,15 @@ local size = 30
 local function bar(n)
     return coroutine.wrap(
         function ()
+            local lastdone = 0
+            io.write("\r["..string.rep("-", size).."]")
             for i = 1, n do
                 local done = math.floor(i * size / n)
-                local rest = size - done
-                io.write("\r["..string.rep("#", done)..string.rep("-", rest).."]")
-                io.flush()
+                if done ~= lastdone then
+                    io.write("\r["..string.rep("#", done))
+                    io.flush()
+                    lastdone = done
+                end
                 coroutine.yield(i)
             end
             io.write("\n")
